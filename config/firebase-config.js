ca33edd04caa73bdf3fc47a9b09e5dd386a42c36
Leonardo Sardella <lsardella@securitasargentina.com>
2025-08-03 10:57:26 -0300

    Fix: integración Firebase Storage usando variables de entorno (clave formateada)

--- config/firebase-config.js
+++ config/firebase-config.js
@@ -8,7 +8,7 @@ admin.initializeApp({
     private_key: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, '\n'),
     client_email: process.env.FIREBASE_CLIENT_EMAIL,
   }),
-  storageBucket: 'etnya.appspot.com'
+  storageBucket: 'etnya-652be.appspot.com' // tu bucket real
 });
 
 const bucket = admin.storage().bucket();
