05719602b7f51d9c8164e618bf16ec3e94d08976
Leonardo Sardella <lsardella@securitasargentina.com>
2025-08-02 22:07:58 -0300

    Refactor: usar variables de entorno para Firebase admin en Render (sin JSON)

--- config/firebase-config.js
+++ config/firebase-config.js
@@ -1,13 +1,15 @@
 const admin = require('firebase-admin');
-const path = require('path');
-
-const serviceAccount = require('./serviceAccountKey.json');
 
 admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount),
-  storageBucket: 'etnya-652be.firebasestorage.app' // cambiá esto si tu bucket es distinto
+  credential: admin.credential.cert({
+    type: process.env.FIREBASE_TYPE,
+    project_id: process.env.FIREBASE_PROJECT_ID,
+    private_key_id: process.env.FIREBASE_PRIVATE_KEY_ID,
+    private_key: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, '\n'),
+    client_email: process.env.FIREBASE_CLIENT_EMAIL,
+  }),
+  storageBucket: 'etnya.appspot.com'
 });
 
 const bucket = admin.storage().bucket();
-
 module.exports = bucket;
