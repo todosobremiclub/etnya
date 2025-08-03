e1ad12b18704f9551601bbdf3f463f6613efa726
Leonardo Sardella <lsardella@securitasargentina.com>
2025-08-03 11:30:35 -0300

    Refactor: conexión Firebase usando variables de entorno con process.env

--- config/firebase-config.js
+++ config/firebase-config.js
@@ -1,12 +1,15 @@
 const admin = require('firebase-admin');
-const serviceAccount = require('./serviceAccountKey.json');
 
 admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount),
-  storageBucket: 'etnya-652be.firebasestorage.app' // ⚠️ tu bucket real
+  credential: admin.credential.cert({
+    type: process.env.FIREBASE_TYPE,
+    project_id: process.env.FIREBASE_PROJECT_ID,
+    private_key_id: process.env.FIREBASE_PRIVATE_KEY_ID,
+    private_key: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, '\n'),
+    client_email: process.env.FIREBASE_CLIENT_EMAIL,
+  }),
+  storageBucket: 'etnya-652be.appspot.com'
 });
 
 const bucket = admin.storage().bucket();
 module.exports = bucket;
-
-
